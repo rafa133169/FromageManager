bf9f895c2a5e793e6b8fe4474ac91cdda4fea1b3
eduardo87DR <spixerspi@gmail.com>
2023-11-01 12:52:27 -0500

    sisi dashboard y navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from './Pages/Dashboard';
+import AppNavbar from './Pages/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <AppNavbar />
+        <Dashboard />
+        <Routes>
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          {/* Otras rutas de tu aplicación */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
