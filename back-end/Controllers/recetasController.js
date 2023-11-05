53f9eb6b015b0b8646cd8bcdf6bfa8efe54b4461
DiegoAle6 <diegoalemanmena@gmail.com>
2023-11-04 22:52:15 -0500

    diego

--- back-end/Controllers/recetasController.js
+++ back-end/Controllers/recetasController.js
@@ -1,11 +1,11 @@
 const connection = require("../database");
 
 const obtenerRecetas = (req, res) => {
-  connection.query("SELECT * FROM Recetas", (error, results) => {
+  connection.query("SELECT * FROM Receta", (error, results) => {
     if (error) {
-      console.error("Error al obtener las recetas", error);
+      console.error("Error al obtener las Receta", error);
       res.status(500).json({
-        error: "Error al obtener las recetas",
+        error: "Error al obtener las Receta",
       });
     } else {
       res.json(results);
@@ -15,7 +15,7 @@ const obtenerRecetas = (req, res) => {
 
 const obtenerRecetaPorId = (req, res) => {
   const id = req.params.id_receta;
-  connection.query("SELECT * FROM Recetas WHERE id_receta = ?", [id], (error, results) => {
+  connection.query("SELECT * FROM Receta WHERE id_receta = ?", [id], (error, results) => {
     if (error) {
       res.status(500).json({ error: "Ocurrió un error al obtener la receta" });
     } else if (results.length === 0) {
@@ -40,7 +40,7 @@ const crearReceta = (req, res) => {
   } = req.body;
 
   connection.query(
-    "INSERT INTO Recetas (queso, id_materiaPrima_id, tiempo_cuajado, tiempo_maduracion, cantidad_sal, prensado, tiempo_total_elaboracion, valoracion_queso, imagen) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    "INSERT INTO Receta (queso, id_materiaPrima_id, tiempo_cuajado, tiempo_maduracion, cantidad_sal, prensado, tiempo_total_elaboracion, valoracion_queso, imagen) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
     [
       queso,
       id_materiaPrima_id,
@@ -80,7 +80,7 @@ const actualizarRecetaPorId = (req, res) => {
   } = req.body;
 
   connection.query(
-    "UPDATE Recetas SET queso = ?, id_materiaPrima_id = ?, tiempo_cuajado = ?, tiempo_maduracion = ?, cantidad_sal = ?, prensado = ?, tiempo_total_elaboracion = ?, valoracion_queso = ?, imagen = ? WHERE id_receta = ?",
+    "UPDATE Receta SET queso = ?, id_materiaPrima_id = ?, tiempo_cuajado = ?, tiempo_maduracion = ?, cantidad_sal = ?, prensado = ?, tiempo_total_elaboracion = ?, valoracion_queso = ?, imagen = ? WHERE id_receta = ?",
     [
       queso,
       id_materiaPrima_id,
@@ -106,7 +106,7 @@ const actualizarRecetaPorId = (req, res) => {
 
 const eliminarRecetaPorId = (req, res) => {
   const id = req.params.id_receta;
-  connection.query("DELETE FROM Recetas WHERE id_receta = ?", [id], (error, results) => {
+  connection.query("DELETE FROM Receta WHERE id_receta = ?", [id], (error, results) => {
     if (error) {
       res.status(500).json({ error: "Ocurrió un error al eliminar la receta" });
     } else {
