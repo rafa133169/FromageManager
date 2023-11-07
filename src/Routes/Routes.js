ca979ea96429c3c12378fc4d84f24a68ff8a3889
rafa133169 <127886555+rafa133169@users.noreply.github.com>
2023-11-07 01:44:38 -0500

    usuarios Back-front

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,6 +3,10 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../Pages/Login"
 import '../App.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Recetas from "../Pages/Recetas";
 import NewReceta from "../Pages/NewReceta";
 import Container from "../Pages/Container";
@@ -10,6 +14,7 @@ import Inventario from "../Pages/Inventario";
 import NewUsuario from "../Pages/NewUsuario";
 import NewIngrediente from "../Pages/NewIngrediente";
 import Usuarios from "../Pages/Usuarios";
+import RectanguloNaranja from "../Components/Elemento.js";
 
 
 export const rutas = createBrowserRouter([
@@ -23,7 +28,7 @@ export const rutas = createBrowserRouter([
   },
    {
      path:"/prueba",
-     element:<Container/>,
+     element:<RectanguloNaranja/>,
    },
   {
     path:"/inventario",
